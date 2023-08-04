62eb83ae7203f4a44e897617829f4ebdf5cc611f
Vaishnavi Saindane <71937373+vaishnavisaindane@users.noreply.github.com>
2023-08-04 10:12:54 +0530

    Create xor.py

---
+++ xor.py
@@ -0,0 +1,27 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+# Prepare the dataset
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
+y = np.array([0, 1, 1, 0])
+
+# Define the model using Keras Sequential API
+model = keras.Sequential([
+    keras.layers.Dense(8, activation='relu', input_shape=(2,)),  # Input layer with 8 neurons and ReLU activation
+    keras.layers.Dense(1, activation='sigmoid')   # Output layer with 1 neuron and Sigmoid activation
+])
+
+# Compile the model
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(X, y, epochs=1000, verbose=0)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X, y)
+print(f'Loss: {loss:.4f}, Accuracy: {accuracy:.4f}')
+
+# Make predictions
+predictions = model.predict(X)
+print('Predictions:', np.round(predictions).flatten())
